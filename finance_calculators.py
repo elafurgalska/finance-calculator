90bed49f2e602abd0e75b6fd251aa807646275a1
elafurgalska <157430359+elafurgalska@users.noreply.github.com>
2024-02-25 16:04:06 +0000

    Add files via upload

---
+++ finance_calculators.py
@@ -0,0 +1,40 @@
+import math
+
+finance_type = input("Welcome to finance calculator! To begin, please choose your calculation from either 'bond' or 'investment': ").lower()
+
+if finance_type == "investment":
+    print("You have chosen: investment")
+    deposit = float(input("Enter the amount of your deposit: "))
+    interest = float(input("Enter your interest: "))
+    years = float(input("Enter the number of years you plan on investing: "))
+    investment_type = input("Choose your investment type: 'compound' or 'simple': ").lower()
+
+    P = deposit
+    t = years
+    r = interest / 100
+
+    if investment_type == "simple":
+        total_sum = P * (1 + r * t)
+        print(f"Your total after {years} years of investment would be {total_sum}")
+
+    elif investment_type == "compound":
+        total_sum = P * math.pow((1 + r), t)
+        print(f"Your total after {years} years of investment would be {total_sum}")
+
+    else:
+        print("Invalid investment type. Please choose 'compound' or 'simple'.")
+
+elif finance_type == "bond":
+    print("You have chosen: bond")
+    house_value = float(input("Enter the starting house value: "))
+    interest_rate = float(input("Enter the interest rate value: "))
+    repayment_months = int(input("Enter the number of months of the bond repayment: "))
+    P = house_value
+    i = interest_rate / 100
+    n = repayment_months
+    repayment = (i * P)/(1 - (1 + i)**(-n))
+    print(f"This is your repayment total: {repayment}")
+
+
+else:
+    print("Invalid finance type. Please choose 'bond' or 'investment'.")
